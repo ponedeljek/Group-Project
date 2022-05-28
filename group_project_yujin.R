b3662aa014c571fdffededbd56e80a3b89ced95c
unknown <1709037@donga.ac.kr>
2022-05-28 20:15:25 +0900

    finished 0528

--- group_project_yujin.R
+++ group_project_yujin.R
@@ -23,7 +23,8 @@ file1 <- original %>%
          "월평균지출_주거비비율",
          "월평균지출_송금금액비율",
          "월평균지출_저축금액비율",
-         "월평균지출_기타금액비율") %>% 
+         "월평균지출_기타금액비율",
+         "X7차직업분류재분류코드") %>% 
   rename(country = "응답자국적코드", 
          marriage = "배우자혼인상태코드", 
          partner_kor_living = "배우자한국거주여부", 
@@ -34,7 +35,8 @@ file1 <- original %>%
          housing_rate = "월평균지출_주거비비율",
          remit_rate = "월평균지출_송금금액비율",
          saving_rate = "월평균지출_저축금액비율",
-         etc_rate = "월평균지출_기타금액비율") %>% 
+         etc_rate = "월평균지출_기타금액비율",
+         job = "X7차직업분류재분류코드") %>% 
   filter(country == 2)
 
 # 기초통계량 요약 : 분석에 사용된 변수들의 관측치, 평균, 표준편차, 최솟값, 최댓값 등
@@ -64,6 +66,9 @@ remittance8 <- ((1373/(271+643+426+539+1085+1192+1093+1373)))*100
 freqRemittance <- xtabs(~remittance, data = file1); freqRemittance
 result_freqremittance <- proportions(freqRemittance) * 100
 
+# living_rate 비중 구하기 
+freqLivingrate <- xtabs(~file2$living_rate, data = file2); freqLivingrate
+result_frqlivingrate <- proportions(freqLivingrate)* 100
 
 # 평균
 file2 <-file1 %>%  filter(is.na(file1$remit_rate)==F &
@@ -108,10 +113,42 @@ max(file2$etc_rate)
 median(file2$etc_rate)
 quantile(file2$etc_rate)
 
+summary(file2$etc_rate)
 
 # 시각화 관련 : 산점도 1개, 선 그래프 1개, 막대 그래프 1개, 지도 그래프 1개 
+# 막대그래프
+file1<-file1%>%filter(is.na(file1$earning)==F)
+g<-dummy_cols(file1,
+              select_columns = 'earning',
+              remove_selected_columns = F)
+ggplot(file1,
+       aes(x=earning))+geom_histogram(binwidth = 0.5)
 
+# 송금금액 or 비율  - 지도
 
+# 
+file1<-dummy_cols(file1, select_columns='job',
+                      remove_selected_columns=F)
+# ggplot(file1,
+#       aes(x=job$job, y=remit_rate))+geom_point()
+
+ggplot(g,
+       aes(x=earning, y=remit_rate))+geom_point()
 
 # 회귀분석 : 모형 선정의 근거, 결과표(단순선형회귀모형 1개, 다중선형회귀모형 2개)
+g%>%
+  lm(remit_rate~earning_1+earning_2+earning_4, .)
+g%>%
+  lm(living_rate~earning_1+earning_2+earning_4, .)
+g%>%
+  lm(housing_rate~earning_1+earning_2+earning_4, .)
+g%>%
+  lm(saving_rate~earning_1+earning_2+earning_4, .)
+g%>%
+  lm(etc_rate~earning_1+earning_2+earning_4, .)
+
+
+
+
+# 소득1구간은 3구간에 비해서 송금비율이 0.28만큼 작은 경향이 있다.
 
